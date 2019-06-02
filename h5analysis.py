0de98166ce9de80a663880d1f82ae1c41697a79d
Jason Dekarske <jdekarske@wisc.edu>
2019-06-01 17:17:33 -0700

    didn't save the last

--- h5analysis.py
+++ h5analysis.py
@@ -5,6 +5,8 @@ import csv
 import numpy as np
 
 
+plotflag = False
+
 directory = os.path.abspath('data/2')
 for subdir, _, files in os.walk(directory):
     if not files:
@@ -19,7 +21,7 @@ for subdir, _, files in os.walk(directory):
             csvlist.append(row)
 
     f = h5py.File(h5filename, 'r')
-    for key, trial in f.items():
+    for i, (key, trial) in enumerate(f.items()):
         emg = np.array(trial)
        
         n = 1000  # number of fft pts
@@ -31,14 +33,17 @@ for subdir, _, files in os.walk(directory):
         posfreq = freq[:int(n / 2)]
         pospower = power[:, :int(n / 2)]
 
-        plt.figure()
-        plt.subplot(121)
-        plt.plot(emg.flatten())
-        plt.subplot(122)
-        plt.plot(posfreq, pospower.flatten())
-        plt.ylim((0, .05))
-        plt.xlim(right=400)
-        xfreqs = [40, 60, 80, 100]
-        for xc in xfreqs:
-            plt.axvline(x=xc, color='m')
-        plt.show()
+        if plotflag:
+            plt.figure()
+            plt.subplot(121)
+            plt.plot(emg.flatten())
+            plt.subplot(122)
+            plt.plot(posfreq, pospower.flatten())
+            plt.ylim((0, .06))
+            plt.xlim(right=400)
+            xfreqs = [40, 60, 80, 100]
+            for xc in xfreqs:
+                plt.axvline(x=xc, color='m')
+            plt.show()
+
+# https://docs.scipy.org/doc/numpy/reference/generated/numpy.corrcoef.html normalized covariance matrix (Pearson)
